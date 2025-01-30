eba53551bdbd89dfea2783c27d2aee55726984bf
BrynOvalles <brynovallesburgos@gmail.com>
2025-01-29 20:18:34 -0400

    Hilt

---
+++ app/src/main/java/edu/ucne/bryanovalles_p1_ap2/BryanOvalles_P1_AP2App.kt
@@ -0,0 +1,8 @@
+package edu.ucne.bryanovalles_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BryanOvalles_P1_AP2App : Application() {
+}
